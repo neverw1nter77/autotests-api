b4cd682451e802f90a24114c376c583f9641397d
neverw1nter77 <neverw1nter100@outlook.com>
2025-10-01 10:29:30 +0700

    Практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field, EmailStr
+
+class UserSchema(BaseModel):
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+    def get_full_name(self) -> str:
+        parts = [self.first_name, self.middle_name, self.last_name]
+        return " ".join([p for p in parts if p])
+class CreateUserRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+class CreateUserResponseSchema(BaseModel):
+    user: UserSchema
+
