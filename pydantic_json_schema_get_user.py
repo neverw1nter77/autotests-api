189e3a769e6b6cfc73ce2b81d74fd7ceab86e937
neverw1nter77 <neverw1nter100@outlook.com>
2025-10-10 12:37:33 +0700

    Практикуемся в работе с JSON schema

--- pydantic_json_schema_get_user.py
+++ pydantic_json_schema_get_user.py
@@ -1,7 +1,7 @@
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.private_users_client import get_private_user_client
 from clients.users.public_users_client import get_public_users_client
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, GetUserResponseSchema
 from tools.assertions.schema import validate_json_schema
 from tools.fakers import get_random_email
 
@@ -16,21 +16,13 @@ create_user_request = CreateUserRequestSchema(
 )
 create_user_response = public_users_client.create_user(create_user_request)
 
-validate_json_schema(
-    instance=create_user_response.model_dump(by_alias=True),
-    schema=CreateUserResponseSchema.model_json_schema()
-)
-
 authentication_user = AuthenticationUserSchema(
     email=create_user_request.email,
     password=create_user_request.password
 )
-private_user_client = get_private_user_client(authentication_user)
+private_users_client = get_private_user_client(authentication_user)
 
-get_user_response = private_user_client.get_user(create_user_response.user.id)
-print("Get user data:", get_user_response)
+get_user_response = private_users_client.get_user_api(create_user_response.user.id)
+get_user_response_schema = GetUserResponseSchema.model_json_schema()
 
-validate_json_schema(
-    instance=get_user_response.model_dump(by_alias=True),
-    schema=GetUserResponseSchema.model_json_schema()
-)
+validate_json_schema(instance=get_user_response.json(), schema=get_user_response_schema)
