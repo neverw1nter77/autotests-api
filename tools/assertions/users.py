6be561be8f8fbe10b94991293c9fc678948e485c
neverw1nter77 <neverw1nter100@outlook.com>
2026-02-13 09:29:44 +0700

    Практика написания автотестов с использованием фикстур

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,4 +1,5 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema, \
+    GetUserResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -14,3 +15,13 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
     assert_equal(response.user.last_name, request.last_name, "last_name")
     assert_equal(response.user.first_name, request.first_name, "first_name")
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
+
+def assert_user(actual: UserSchema, expected: UserSchema):
+    assert_equal(actual.id, expected.id, "User id mismatch")
+    assert_equal(actual.email, expected.email, "User email mismatch")
+    assert_equal(actual.last_name, expected.last_name, "User last_name mismatch")
+    assert_equal(actual.first_name, expected.first_name, "User first_name mismatch")
+    assert_equal(actual.middle_name, expected.middle_name, "User middle_name mismatch")
+
+def assert_get_user_response(get_user_response: GetUserResponseSchema, create_user_response: CreateUserResponseSchema):
+    assert_user(actual=get_user_response.user, expected=create_user_response.user)
