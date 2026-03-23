8487c47e3e72d581ce3ba694ce5c4aaee77e0ffa
neverw1nter77 <neverw1nter100@outlook.com>
2026-03-23 09:33:03 +0700

    Практика написания API автотестов для работы с заданиями. Получение задания

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,4 +1,5 @@
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    GetExerciseResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -17,3 +18,20 @@ def assert_create_exercise_response(request: CreateExerciseRequestSchema, respon
     assert_equal(response.exercise.order_index, request.order_index, "order_index")
     assert_equal(response.exercise.description, request.description, "description")
     assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
+
+def assert_exercise(actual, expected):
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.course_id, expected.course_id, "course_id")
+    assert_equal(actual.max_score, expected.max_score, "max_score")
+    assert_equal(actual.min_score, expected.min_score, "min_score")
+    assert_equal(actual.order_index, expected.order_index, "order_index")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+
+
+def assert_get_exercise_response(
+        actual: GetExerciseResponseSchema,
+        expected: CreateExerciseResponseSchema
+):
+    assert_exercise(actual.exercise, expected.exercise)
