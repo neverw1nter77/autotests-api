4be8d2a98896ae6f09f7e5d1e87fcdd811874e2d
neverw1nter77 <neverw1nter100@outlook.com>
2026-05-01 09:26:00 +0700

    Улучшение работы с API клиентами с помощью Enum для URI

--- clients/authentication/authentication_client.py
+++ clients/authentication/authentication_client.py
@@ -3,6 +3,7 @@ import allure
 from clients.api_client import APIClient
 from clients.public_https_builder import get_public_http_client
 from clients.authentication.authentication_schema import LoginRequestSchema, RefreshRequestSchema, LoginResponseSchema
+from tools.routes import APIRoutes
 
 class AuthenticationClient(APIClient):
     """
@@ -18,7 +19,7 @@ class AuthenticationClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post(
-            "/api/v1/authentication/login",
+            f"{APIRoutes.AUTHENTICATION}/login",
             json=request.model_dump(by_alias=True)
         )
 
@@ -31,7 +32,7 @@ class AuthenticationClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post(
-            "/api/v1/authentication/refresh",
+            f"{APIRoutes.AUTHENTICATION}/refresh",
             json=request.model_dump(by_alias=True))
 
     def login(self, request: LoginRequestSchema) -> LoginResponseSchema:
--- clients/users/private_users_client.py
+++ clients/users/private_users_client.py
@@ -3,6 +3,7 @@ from httpx import Response
 import allure
 from clients.private_http_builder import get_private_http_client, AuthenticationUserSchema
 from clients.users.users_schema import GetUserResponseSchema, UpdateUserRequestSchema
+from tools.routes import APIRoutes
 
 """ЛОГИНИТСЯ этим же пользователем созданным в PublicUsersClient и ДЕЛАЕТ запросы от его имени"""
 class PrivateUsersClient(APIClient):
@@ -16,7 +17,7 @@ class PrivateUsersClient(APIClient):
         Метод получения текущего пользователя.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get("/api/v1/users/me")
+        return self.get(f"{APIRoutes.USERS}/me")
 
     @allure.step("Get user by id {user_id}")
     def get_user_api(self, user_id: str) -> Response:
@@ -26,7 +27,7 @@ class PrivateUsersClient(APIClient):
         :param user_id: Идентификатор пользователя.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get(f"/api/v1/users/{user_id}")
+        return self.get(f"{APIRoutes.USERS}/{user_id}")
 
     @allure.step("Update user by id {user_id}")
     def update_user_api(self, user_id: str, request: UpdateUserRequestSchema) -> Response:
@@ -37,7 +38,7 @@ class PrivateUsersClient(APIClient):
         :param request: Словарь с email, lastName, firstName, middleName.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.patch(f"/api/v1/users/{user_id}", json=request.model_dump(by_alias=True))
+        return self.patch(f"{APIRoutes.USERS}/{user_id}", json=request.model_dump(by_alias=True))
 
     @allure.step("Delete user by id {user_id}")
     def delete_user_api(self, user_id: str) -> Response:
