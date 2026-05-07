3534aac35815ccf778f8269dc6d2260d4953d8a7
neverw1nter77 <neverw1nter77@users.noreply.github.com>
2026-05-07 02:48:13 +0000

    deploy: 3d9ca5ac1efe05a7ef44b76e37a07135924e9f8e

--- conftest.py
+++
@@ -1,8 +0,0 @@
-pytest_plugins = (
-    "fixtures.users",
-    "fixtures.files",
-    "fixtures.courses",  # Добавляем фикстуры для работы с курсами
-    "fixtures.exercises",
-    "fixtures.authentication",
-    "fixtures.allure"
-    )
